6bb97b82c3e655337eb724aef0120154957d61d6
Sam516-Dev <96120597+Sam516-Dev@users.noreply.github.com>
2021-12-17 11:55:27 +0000

    nav-bar bg rectangle
    Signed-off-by: Sam516-Dev <96120597+Sam516-Dev@users.noreply.github.com>

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavbarContainer} from "../components/Navbar.styles";
+function Navbar (){
+    return (
+         <NavbarContainer>  </NavbarContainer>
+
+    );
+    
+   
+}
+
+export default Navbar;
---
+++ src/components/Navbar.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+width: 100%;
+height: 80px;
+background-color: black;
+display: flex;
+flex-direction: column;
+
+`;
